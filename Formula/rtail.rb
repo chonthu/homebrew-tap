052692494c8b7dc90957085654107460d76b4419
Nithin Meppurathu <admin@nitmedia.com>
2017-11-01 22:30:19 -0400

    Update rtail.rb

--- Formula/rtail.rb
+++ Formula/rtail.rb
@@ -1,9 +1,9 @@
 class Rtail < Formula
   desc "Remote tailing tool"
   homepage "https://github.com/chonthu/rtail"
-  url "https://github.com/chonthu/rtail/releases/download/2.2.3/rtail.gz"
-  version "2.2.3"
-  sha256 "1f554675219cd11316be6024453d5e4ee55a4110af0e9293f0556dbbfdd26e39"
+  url "https://github.com/chonthu/rtail/releases/download/2.2.4/rtail.gz"
+  version "2.2.4"
+  sha256 "d842da6b36ee8ab2d44893f3eaa464dd8f1040a154832ab0a11987586750676d"
 
   depends_on :arch => :intel
 
